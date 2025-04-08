a669e79772836463dbc394348f6ee3e66e2497fc
lllmaclll <makky1593@gmail.com>
2025-04-08 22:08:30 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline bg-sky-950">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
