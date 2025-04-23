ffbeb6fc7f975562a98098fc5a085938aa3e0868
lllmaclll <makky1593@gmail.com>
2025-04-23 22:12:22 +0700

    install daisyui

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
       <h1 className="text-3xl font-bold underline bg-sky-950">
         Hello world!
       </h1>
+      <button className="btn btn-secondary">Secondary</button>
     </>
   )
 }
